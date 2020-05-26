96810c76bbbfa3cc95e5de47fe7b80563bc1775b
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2020-05-26 23:48:29 +0300

    Move settings.py to settings package, create development settings

---
+++ optimus_type/settings/development.py
@@ -0,0 +1,10 @@
+from .base import *  # noqa
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'p(#03tt!#wmd(+pav5f&u&&4wc6c34+hno++3+q-)i&%v_vr6*'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
