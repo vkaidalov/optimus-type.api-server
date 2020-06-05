53eba08ff045e66569c24e853a233c585a17617b
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2020-06-05 09:48:35 +0300

    POST, DELETE exercise, set permissions and the "is_removed" logic

--- exercises/serializers.py
+++ exercises/serializers.py
@@ -4,11 +4,7 @@ from .models import Exercise
 
 
 class ExerciseSerializer(serializers.ModelSerializer):
-    locale = serializers.SerializerMethodField()
-
     class Meta:
         model = Exercise
         fields = ('id', 'creator', 'created_at', 'locale', 'title', 'content')
-
-    def get_locale(self, obj):  # noqa
-        return obj.get_locale_display()
+        read_only_fields = ('id', 'creator', 'created_at')
--- exercises/views.py
+++ exercises/views.py
@@ -1,15 +1,42 @@
 from django.utils.decorators import method_decorator
 from drf_yasg.utils import swagger_auto_schema
+from rest_framework import mixins
 from rest_framework import viewsets
+from rest_framework import permissions
 
 from .models import Exercise
 from .serializers import ExerciseSerializer
+from .permissions import IsCreatorOrReadOnly
 
 
 @method_decorator(
-    name='list', decorator=swagger_auto_schema(tags=['exercises']))
+    name='create', decorator=swagger_auto_schema(tags=['exercises'])
+)
 @method_decorator(
-    name='retrieve', decorator=swagger_auto_schema(tags=['exercises']))
-class ExerciseViewSet(viewsets.ReadOnlyModelViewSet):
+    name='list', decorator=swagger_auto_schema(tags=['exercises'])
+)
+@method_decorator(
+    name='retrieve', decorator=swagger_auto_schema(tags=['exercises'])
+)
+@method_decorator(
+    name='destroy', decorator=swagger_auto_schema(tags=['exercises'])
+)
+class ExerciseViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = Exercise.objects.filter(is_banned=False, is_removed=False)
     serializer_class = ExerciseSerializer
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly, IsCreatorOrReadOnly
+    ]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    def perform_destroy(self, instance: Exercise):
+        instance.is_removed = True
+        instance.save()
